be7643353a22e328066191eea41f218ba11d1a16
Jonathan Hsieh <jonathanh1386@gmail.com>
2020-08-11 04:45:42 -0700

    can find the cells of board

--- find.py
+++ find.py
@@ -10,18 +10,18 @@ def get_anchor_location():
             continue
         return anchor_location
 
-
-anc_loc = get_anchor_location()
-pyautogui.moveTo(anc_loc.left, anc_loc.top)
-
 def get_cell_locations():
     EASY_NUM, MED_NUM, HARD_NUM = 32, 111, 219 # the number of intersections for each difficulty
-    cell_width, cell_height = -1, -1
+    EASY_SIZE = (10, 8)
+    MED_SIZE = (18, 14)
+    HARD_SIZE = (24, 20)
 
-    rtn_pos = []
+    cell_size = -1
+    board_size = (-1, -1)
+
     intersections = list(pyautogui.locateAllOnScreen("intersect.png"))
 
-    mi_x, mx_x, mi_y, mx_y = -1, -1, -1, -1
+    mi_x, mx_x, mi_y, mx_y = 10**10, -1, 10**10, -1
     for i in intersections:
         mi_x = min(mi_x, i.left)
         mx_x = max(mx_x, i.left + i.width)
@@ -29,22 +29,27 @@ def get_cell_locations():
         mi_y = min(mi_y, i.top)
         mx_y = max(mx_y, i.top + i.height)
 
-    intersections.append(2)
     if(len(intersections) == EASY_NUM):
-        cell_width = (mx_x - mi_x) // 10
-        cell_height = (mx_y - mi_y) // 8
+        cell_size = 50
+        board_size = EASY_SIZE
     elif(len(intersections) == MED_NUM):
-        cell_width = (mx_x - mi_x) // 18
-        cell_height = (mx_y - mi_y) // 14
+        cell_size = 30
+        board_size = MED_SIZE
     elif(len(intersections) == HARD_NUM):
-        cell_width = (mx_x - mi_x) // 24
-        cell_height = (mx_y - mi_y) // 20
+        cell_Size = 15
+        board_size = HARD_SIZE
     else:
         print("Invalid number of intersects found!!!")
         return []
 
 
+    rtn_pos = [[] for _ in range(board_size[1])]
+    for i in range(board_size[1]):
+        for j in range(board_size[0]):
+            rtn_pos[i].append((mi_x + (j*cell_size), mi_y + (i*cell_size)))
 
-get_cell_locations()
+    return rtn_pos
+
+cell_pos = get_cell_locations()
 
 
